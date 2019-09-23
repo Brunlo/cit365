ed55110d1081b229c1c81bd98120c7b8f08a5293
Logan Cratty <logancratty1@gmail.com>
2019-09-23 14:48:08 -0600

    commit

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -45,34 +45,41 @@ namespace ConsoleApplication
                 double width, height, woodLength, glassArea;
                 string widthString, heightString;
 
-                widthString = Console.ReadLine();
-                width = double.Parse(widthString);
-                heightString = Console.ReadLine();
-                height = double.Parse(heightString);
-                woodLength = 2 * (width + height) * 3.25;
-                glassArea = 2 * (width * height);
-                Console.WriteLine("The length of the wood is " +
-                woodLength + " feet");
-                Console.WriteLine("The area of the glass is " +
-                glassArea + " square metres");
+               
+
+
+              
 
 
 
 
             //7
-            //Console.Write("Enter the width and press <Enter>");
-            //widthString = Console.ReadLine();
-            // width = double.Parse(widthString);
+            Console.Write("Enter the width and press <Enter>");
+            widthString = Console.ReadLine();
+             width = double.Parse(widthString);
 
-            //Console.Write("Enter the height and press <Enter>");
-            //heightString = Console.ReadLine();
-            // height = double.Parse(heightString);
+            Console.Write("Enter the height and press <Enter>");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
 
-            //woodLength = 2 * (width + height) * 3.25;
-            //glassArea = 2 * (width * height);
+
+
+
+
+            //Calculations
+            Console.Write("Results <>");
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+
 
             Console.WriteLine("Press any key to end: ");
             Console.ReadKey();
+
+
 
         }
     }
